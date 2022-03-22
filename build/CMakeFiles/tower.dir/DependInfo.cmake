224ca7c7a2c53f623a6df071ad6cd1183b15e914
scowlingmask419 <ajaafri1017@gmail.com>
2022-03-22 10:35:36 +0100

    First commit

---
+++ build/CMakeFiles/tower.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amyr/Bureau/CPP_Learning_Project/src/GL/opengl_interface.cpp" "/home/amyr/Bureau/CPP_Learning_Project/build/CMakeFiles/tower.dir/src/GL/opengl_interface.cpp.o"
+  "/home/amyr/Bureau/CPP_Learning_Project/src/aircraft.cpp" "/home/amyr/Bureau/CPP_Learning_Project/build/CMakeFiles/tower.dir/src/aircraft.cpp.o"
+  "/home/amyr/Bureau/CPP_Learning_Project/src/img/image.cpp" "/home/amyr/Bureau/CPP_Learning_Project/build/CMakeFiles/tower.dir/src/img/image.cpp.o"
+  "/home/amyr/Bureau/CPP_Learning_Project/src/main.cpp" "/home/amyr/Bureau/CPP_Learning_Project/build/CMakeFiles/tower.dir/src/main.cpp.o"
+  "/home/amyr/Bureau/CPP_Learning_Project/src/tower.cpp" "/home/amyr/Bureau/CPP_Learning_Project/build/CMakeFiles/tower.dir/src/tower.cpp.o"
+  "/home/amyr/Bureau/CPP_Learning_Project/src/tower_sim.cpp" "/home/amyr/Bureau/CPP_Learning_Project/build/CMakeFiles/tower.dir/src/tower_sim.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLUT_DISABLE_ATEXIT_HACK"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/img/media_path.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+class Path
+{
+private:
+#ifdef _WIN32
+    static inline const char SEPARATOR = '\\';
+    static inline const char ALT_SEPARATOR = '/';
+#else
+    static inline const char SEPARATOR = '/';
+    static inline const char ALT_SEPARATOR = '/';
+#endif
+
+    std::string value;
+
+public:
+    Path(const std::string_view value_ = {}) : value { value_ }
+    {} 
+
+    bool empty() const
+    {
+        return value.empty();
+    }
+
+    Path operator/(const Path& other) const
+    {
+        return Path { value + SEPARATOR + other.value };
+    }
+
+    const std::string& string() const { return value; }
+
+    void replace_filename(const std::string_view filename)
+    {
+        auto pos = value.find_last_of(SEPARATOR);
+        if (pos == std::string::npos)
+        {
+            pos = value.find_last_of(ALT_SEPARATOR);
+        }
+
+        value.replace(pos + 1, std::string::npos, filename);
+    }
+};
+
+class MediaPath
+{
+public:
+    static inline void initialize(const std::string_view executable_path)
+    {
+        media_path = executable_path;
+        media_path.replace_filename("media");
+    }
+
+    MediaPath(const std::string_view path_) : path { path_ } {}
+
+    Path get_full_path() const
+    {
+        if (media_path.empty())
+        {
+            throw std::runtime_error { "MediaPath::initialize() should have been called before "
+                                       "trying to retrieve a path from MediaPath." };
+        }
+
+        return media_path / path;
+    }
+
+private:
+    static inline Path media_path;
+
+    const Path path;
+};
